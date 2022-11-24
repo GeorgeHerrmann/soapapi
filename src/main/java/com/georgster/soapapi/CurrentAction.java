e0ef6651fecf9b4aa253cbb36f123f60340cf0da
George Herrmann <gvhmann@gmail.com>
2022-11-24 17:03:03 -0500

    Updated current action to temp read from files

--- src/main/java/com/georgster/soapapi/CurrentAction.java
+++ src/main/java/com/georgster/soapapi/CurrentAction.java
@@ -1,9 +1,17 @@
 package com.georgster.soapapi;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class CurrentAction {
     private String action;
-    public CurrentAction(String test) {
-        action = test;
+    public CurrentAction() {
+        try {
+            action = Files.readString( Path.of(System.getProperty("user.dir"), "..", "soapbot", "src", "main", "java", "com", "georgster", "api", "data", "currentaction.json"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getAction() {
