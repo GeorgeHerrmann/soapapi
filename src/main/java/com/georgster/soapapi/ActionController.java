d6822b09552aaa47db37fa8ca5d0fd1ab4ddf6d3
George Herrmann <gvhmann@gmail.com>
2022-11-24 23:15:02 -0500

    Fixed issue with cross mapping causing cors issue

--- src/main/java/com/georgster/soapapi/ActionController.java
+++ src/main/java/com/georgster/soapapi/ActionController.java
@@ -1,10 +1,11 @@
 package com.georgster.soapapi;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="*")
 public class ActionController {
     @GetMapping("/action")
     public CurrentAction currentAction () {
